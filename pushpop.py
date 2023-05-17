8f545382364847f4b0cd7f3f7d826ee73f821442
ARES-AM-RS <rochaksulu2002@gmail.com>
2023-05-17 22:04:12 +0545

    peak size isempty isfull

--- pushpop.py
+++ pushpop.py
@@ -9,32 +9,61 @@ def push(x):
 
     tos+=1
     stackarray.insert(tos,x)
-    print(stackarray)
-    return stackarray
+    # print(stackarray)
+    # return stackarray
 def pop():
+    global tos
     if tos==-1:
         raise IndexError("stack underflow")
     value=stackarray[tos]
     stackarray.pop(tos)
+    tos-=1
     # print(tos)
     return value
+def peek():
+    global tos
+    if tos==-1:
+        raise IndexError("stack underflow")
+    value=stackarray[tos]
+    
+    # tos-=1
+    # print(tos)
+    return value
+
+
         
             
 
 def choicepoporpush(choice):
     if choice==1:
         inputvalue=int(input("Enter the value you want to stack"))
-        print(f"{choicepoporpush(inputvalue)} is stacked")
-        
+        pushval=push(inputvalue)
+        print(f"{inputvalue} is stacked")
+        z=int(input("enter 1 to stack 0 to pop ,2 to quit,3 to peek,4 to check if stack is full,5 to check if stack is empty"))
+        choicepoporpush(z)
         # choicepoporpush(inputvalue)
         
 
     elif choice==0:
-        print(f"{pop()} is poped")
-        z=int(input("enter 1 to stack 0 to pop and 2 to quit"))
+        popedvalue=pop()
+        print(f"{popedvalue} is poped")
+        z=int(input("enter 1 to stack 0 to pop ,2 to quit,3 to peek,4 to check if stack is full,5 to check if stack is empty"))
         choicepoporpush(z)
     elif choice==2:
         print(f"final stack is {stackarray}\nquitting")
+    elif choice==3:
+        peekvalue=peek()
+        print(f"{peekvalue} is in the top of stack")
+        z=int(input("enter 1 to stack 0 to pop ,2 to quit,3 to peek,4 to check if stack is full,5 to check if stack is empty"))
+        choicepoporpush(z)
+    elif choice==4:
+        print("stack is full") if tos==4 else print("stack is not full")
+        z=int(input("enter 1 to stack 0 to pop ,2 to quit,3 to peek,4 to check if stack is full,5 to check if stack is empty"))
+        choicepoporpush(z)
+    elif choice==5:
+        print("stack is empty") if tos==-1 else print("stack is not empty")
+        z=int(input("enter 1 to stack 0 to pop ,2 to quit,3 to peek,4 to check if stack is full,5 to check if stack is empty"))
+        choicepoporpush(z)
     else:
         y=int(input("please enter 0 , 1 or 2  only"))
         choicepoporpush(y)
@@ -43,7 +72,6 @@ def choicepoporpush(choice):
 choice=int(input("enter 1 to push and 0 to pop"))
 
 choicepoporpush(choice)
-print(stackarray)
 
 
     
